97a19717148208a3fcc3d1c689276f49f3a8ef40
LucGaluc <amartinoluc@gmail.com>
2023-10-16 14:53:31 +0200

    commit four

--- src/searchPosts/searchPosts.jsx
+++ src/searchPosts/searchPosts.jsx
@@ -7,50 +7,43 @@ function SearchPosts() {
     const [allPosts, setAllPosts] = useState([]);
     const [filteredPosts, setFilteredPosts] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
 
-    // Fonction de vérification du token
-    const isTokenValid = (token) => {
-        console.log("token", token);
-        const storedToken = localStorage.getItem("token");
-        return token === storedToken;
+    const PAGE_SIZE = 10; // Nombre de posts par page
+
+    const fetchPosts = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(
+                `https://social-network-api.osc-fr1.scalingo.io/serial-viewer/posts?page=${currentPage}&limit=${PAGE_SIZE}`,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            if (response.status === 401) {
+                throw new Error("Non autorisé");
+            }
+            if (response.status !== 200) {
+                throw new Error("Erreur HTTP " + response.status);
+            }
+            const data = await response.json();
+            setAllPosts(data.posts);
+        } catch (error) {
+            console.error("Une erreur s'est produite :", error);
+        } finally {
+            setLoading(false);
+        }
     };
 
-    const fetchAllPosts = () => {
-        setLoading(true);
-        fetch(
-            "https://social-network-api.osc-fr1.scalingo.io/serial-viewer/posts?page=2&limit=10",
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        )
-            .then((response) => {
-                if (response.status === 401) {
-                    // erreur d'authentification
-                    throw new Error("Non autorisé");
-                }
-                if (response.status !== 200) {
-                    // autres erreurs HTTP
-                    throw new Error("Erreur HTTP " + response.status);
-                }
-                return response.json();
-            })
-            .then((data) => {
-                // Vérifier que le token est correct
-
-                setAllPosts(data.posts);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.error("Une erreur s'est produite :", error);
-                setLoading(false);
-            });
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
     };
 
     useEffect(() => {
-        fetchAllPosts();
-    }, []);
+        fetchPosts();
+    }, [currentPage]);
 
     useEffect(() => {
         filterPosts();
@@ -70,7 +63,6 @@ function SearchPosts() {
             });
             setFilteredPosts(filtered);
         } else {
-            // allPosts n'est pas un tableau, définir filteredPosts sur un tableau vide.
             setFilteredPosts([]);
         }
     };
@@ -78,7 +70,6 @@ function SearchPosts() {
     return (
         <div>
             <div className="navContainer">
-                {" "}
                 <NavBar />
             </div>
             <h1>Recherche de Posts</h1>
@@ -102,6 +93,21 @@ function SearchPosts() {
                     </ul>
                 </div>
             )}
+
+            <div className="pagination">
+                <button
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 1}
+                >
+                    Page précédente
+                </button>
+                <button
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    disabled={filteredPosts.length < PAGE_SIZE}
+                >
+                    Page suivante
+                </button>
+            </div>
         </div>
     );
 }
