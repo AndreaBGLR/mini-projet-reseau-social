6ccf7fea521fcee1459fe60f2f3a3be7ec3b45d0
sergioMG98 <sergio.marquesgomes98@gmail.com>
2023-10-17 14:05:39 +0200

    alert sur like

--- src/Post/RecupPosts.jsx
+++ src/Post/RecupPosts.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from "react";
 import "./RecupPosts.css";
-import NavBar from "../Components/navBar/navBar";
+import NavBar from "../components/navBar/navBar";
 
 function RecupPosts() {
-  const apiUrlLike = "https://social-network-api.osc-fr1.scalingo.io/serial-viewer/post/like";
+  const apiUrlLike =
+    "https://social-network-api.osc-fr1.scalingo.io/serial-viewer/post/like";
   const apiUrlCom =
     "https://social-network-api.osc-fr1.scalingo.io/serial-viewer/post/comment";
   const apiUrlRecup =
-    "https://social-network-api.osc-fr1.scalingo.io/serial-viewer/posts?page=1&limit=10";
+    "https://social-network-api.osc-fr1.scalingo.io/serial-viewer/posts?page=1";
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [posts, setPosts] = useState([]);
   const [com, setCom] = useState("");
   // Use local state for comments
   const [commentStates, setCommentStates] = useState({});
   const [like, setLike] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
 
   async function commenter(postId) {
     try {
@@ -62,40 +64,50 @@ function RecupPosts() {
       console.error("Erreur : " + error);
     }
   }
-
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
   useEffect(() => {
     recupPosts();
   }, []);
 
 
-  async function liker(likeId) {
 
-    console.log('efzev' ,likeId);
-    setLike({id: likeId._id, firstname: likeId.firstname, lastname: likeId.lastname })
+  function li(postId, post) {
+    console.log("post", post.likes);
+    post.likes.forEach(element => {
+      console.log("element user", element.userId);
+      console.log("post id", postId);
+       if (element.userId == post.userId) {
+        alert("tu as déja liké connard")
+      }
+    });
+    liker(postId, post)
+  }
 
 
+  async function liker(postId, post) {
+    console.log('liker', postId);
     try {
-        const response = await fetch(apiUrlLike, {
+      const response = await fetch(apiUrlLike, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: "bearer " + token,
         },
-        body :JSON.stringify({
-          postId: like,
-        })
-        })
-        
+        body: JSON.stringify({
+          postId: postId,
+        }),
+      });
+
       if (!response.ok) {
         throw new Error(`Erreur de réseau - ${response.status}`);
       }
 
-      const data = await response.json()
-      (console.log('rver' , like))
-      /* setLike({}) */
-      setLike({...like, [postId]: ""});
+      const data = await response.json();
+      console.log("data", data);
+      setLike({ ...like, [postId]: "" });
       recupPosts();
-
     } catch (error) {
       console.error("Erreur : " + error);
     }
@@ -112,7 +124,6 @@ function RecupPosts() {
           <li key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content}</p>
-
             <div className="comments-section">
               <div>
                 <input
@@ -125,18 +136,31 @@ function RecupPosts() {
                 <button onClick={() => commenter(post._id)} type="button">
                   Commenter
                 </button>{" "}
-                <h2>Commentaires</h2>
+                {post.comments.lenght > 0 && <h2>Commentaires :</h2>}
                 {post.comments?.map((com) => (
                   <p key={com.id}>{com.content}</p>
                 ))}
               </div>
-               {console.log("but",post.likes.length)}
-               <button onClick={() => liker(post)} type="button">Like</button>
-                {"nombre de like : " + post.likes.length}
+              {/* {console.log("but",post)} */}
+              <button onClick={() => li(post._id, post)} type="submit">
+                Like
+              </button>
+              {post.likes.length}
             </div>
           </li>
         ))}
       </ul>
+      <div className="pagination">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Page précédente
+        </button>
+        <button onClick={() => handlePageChange(currentPage + 1)}>
+          Page suivante
+        </button>
+      </div>
     </div>
   );
 }
