a1cd5d339ba4b95321867552e67dfa377f3dc82e
AndreaBGLR <andrea.baglieri@laposte.net>
2023-10-18 09:53:46 +0200

    details

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -3,51 +3,49 @@ import "./navBar.css";
 import Logout from "../../Auth/Logout/Logout";
 
 function NavBar(props) {
-    /* console.log(props.tableu[0]); */
-    return (
-        <div className="nav">
-            <div className="idSite">
-{/*                 <div className="titre">
+  /* console.log(props.tableu[0]); */
+  return (
+    <div className="nav">
+      <div className="idSite">
+        {/*                 <div className="titre">
                     <p>Serial viewer</p>
                 </div> */}
-                <div className="navImage">
-                    <div className="image">
-                        
-                    </div>
-                    <div className="tete"></div>
-                    {/* <img src={""} alt="logo" /> */}
-                </div>
-            </div>
-            <nav className="logout lien">
-                <button className="logout lien">
-                    <Logout />
-                </button>
-            </nav>
-            <nav className="home lien">
-                <Link to={"/"}>Accueil</Link>
-            </nav>
-            <nav className="login lien">
-                <Link to={"/LoginForm.jsx"}>Connexion</Link>
-            </nav>
-            <nav className="register lien">
-                <Link to={"/Register.jsx"}>Creer un Compte</Link>
-            </nav>
-            <nav className="profile lien">
-                <Link to={"/Profile.jsx"}>Editer Profil</Link>
-            </nav>
-            <nav className="search lien">
-                <Link to={"/search"}>Chercher un film</Link>
-            </nav>
-            <nav className="search lien">
-                <Link to={"/searchPosts.jsx"}>Chercher un Post</Link>
-            </nav>
-            <nav className="render lien">
-                <Link to={"/RecupPosts.jsx"}>Afficher Posts</Link>
-            </nav>
-            <nav className="create lien">
-                <Link to={"/CreatePost.jsx"}>Faire un post</Link>
-            </nav>
+        <div className="navImage">
+          <div className="image"></div>
+          <div className="tete"></div>
+          {/* <img src={""} alt="logo" /> */}
         </div>
-    );
+      </div>
+      <nav className="logout lien">
+        <button className="logout lien">
+          <Logout />
+        </button>
+      </nav>
+      <nav className="home lien">
+        <Link to={"/"}>Accueil</Link>
+      </nav>
+      <nav className="login lien">
+        <Link to={"/LoginForm.jsx"}>Connexion</Link>
+      </nav>
+      <nav className="register lien">
+        <Link to={"/Register.jsx"}>Creer un Compte</Link>
+      </nav>
+      <nav className="profile lien">
+        <Link to={"/Profile.jsx"}>Editer Profil</Link>
+      </nav>
+      <nav className="search lien">
+        <Link to={"/search"}>Chercher un film</Link>
+      </nav>
+      <nav className="search lien">
+        <Link to={"/searchPosts.jsx"}>Chercher un Post</Link>
+      </nav>
+      <nav className="render lien">
+        <Link to={"/RecupPosts.jsx"}>Afficher Posts</Link>
+      </nav>
+      <nav className="create lien">
+        <Link to={"/CreatePost.jsx"}>Créer un post</Link>
+      </nav>
+    </div>
+  );
 }
 export default NavBar;
